ab5f29a12665865313a4f4e067a2a271541bfd2c
XiiaoPanda <vccx94@live.com.sg>
2019-10-31 10:40:27 +0800

    driver able to advertise trips

--- App/routes/driver_advertise.js
+++ App/routes/driver_advertise.js
@@ -1,17 +1,41 @@
 const express = require('express');
 const router = express.Router();
+const sql_query = require('../sql');
+var moment = require('moment');
 
 const { Pool } = require('pg')
 const pool = new Pool({
 	connectionString: process.env.DATABASE_URL
 });
 
-/* SQL Query */
-var sql_query = 'SELECT * FROM advertised_trips WHERE driver_username =';
+/* POST */
+router.post('/advertise', (req, res, next) => {
+	var driver_username  = req.signedCookies.user_id;
+    var license_plate = req.body.license_plate;
+	var s_location = req.body.s_location;
+	var e_location = req.body.e_location;
+	var s_time = req.body.s_time;
+	var e_time = req.body.e_time;
+	var s_date = req.body.s_date;
+	var e_date = req.body.e_date;
+	var min_bid = '5';
+	var total_dist = '10';
+	var driver_rating = '5';
+	pool.query(sql_query.query.add_advertised_trips, [driver_username, s_location, e_location, s_time, e_time, s_date, e_date, license_plate, min_bid, total_dist, driver_rating],(err, data2) => {
+		if(err) {
+			console.error(err);
+		}
+		else
+		{
+			res.redirect('/driver_advertise');
+		}
+	});
+});
 
+/* GET */
 router.get('/', function(req, res, next) {
-	var get_user_info = sql_query +  "'" + req.signedCookies.user_id + "'";
-	pool.query(get_user_info, (err, data) => {
+	var driver_username = req.signedCookies.user_id;
+	pool.query(sql_query.query.driver_advertised_trips, [driver_username], (err, data) => {
 		if(err){
             res.json({
                 message : 'ERROR'
--- App/sql/index.js
+++ App/sql/index.js
@@ -8,6 +8,8 @@ sql.query = {
     add_vehicle: "INSERT INTO vehicles VALUES ($1, $2)",
     get_model: "SELECT * FROM category WHERE type=$1",
 
+    add_advertised_trips: "INSERT INTO advertised_trips VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)",
+    driver_advertised_trips: "SELECT * FROM advertised_trips WHERE driver_username = $1",
     all_advertised_trips: "SELECT * FROM advertised_trips",
     add_bid: "INSERT INTO bids VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)",
     display_bids : "SELECT bid_price, passenger_username, driver_username, s_location, e_location, s_time, e_time, s_date, e_date, license_plate, min_bid, is_win FROM bids WHERE passenger_username= $1"
